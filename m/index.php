baee95df1686dca8b7a8680b239ec617df5c2a6e
kyhfan <kyhfan@gmail.com>
2018-12-13 21:19:28 +0900

    22222

--- m/index.php
+++ m/index.php
@@ -473,17 +473,20 @@
 		function viewVideo() {
 			$(".video-thumb-layer").hide();
 			$(".video-layer").show();
-			player.play();			
+			// player.play();			
 		}
-		$('.video-layer').on('click', function() {
+		$('.section1-wrap .inner').on('click', function() {
 			// player.trigger('click');
-			if (player.paused()) {
+			if ($(".video-thumb-layer").css("display") == "none")
+			{
+				if (player.paused()) {
 					player.play();
 					videojs.log('Your player is play!');
 				} else {
 					player.pause();
 					videojs.log('Your player is pause!');
 				}
+			}
 
 		});
 
