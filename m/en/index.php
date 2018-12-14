6761cdcedb9b8a5a208fefdb1663ed0b16d008be
ojoonwoo <ojoonwoo@gmail.com>
2018-12-14 16:55:33 +0900

    글로벌 유튜브 추가

--- m/en/index.php
+++ m/en/index.php
@@ -346,9 +346,12 @@
 				<a href="https://www.instagram.com/hyundai_motorstudio/" target="_blank" class="insta" onclick="click_tracking('외부링크 오피셜인스타그램')">
                     <img src="../images/share_insta.png" alt="">
                 </a>
-				<a href="https://www.youtube.com/user/HyundaiWorldwide" target="_blank" class="youtube" onclick="click_tracking('외부링크 오피셜유튜브')">
+				<a href="https://www.youtube.com/user/AboutHyundai" target="_blank" class="youtube" onclick="click_tracking('외부링크 오피셜유튜브')">
                     <img src="../images/share_youtube.png" alt="">
                 </a>
+				<a href="https://www.youtube.com/user/HyundaiWorldwide" target="_blank" class="youtube-global" onclick="click_tracking('외부링크 글로벌유튜브')">
+					<img src="../images/share_youtube_global.png" alt="">
+				</a>
 				<a href="https://story.kakao.com/ch/hyundai" target="_blank" class="ks" onclick="click_tracking('외부링크 오피셜카카오스토리')">
                     <img src="../images/share_ks.png" alt="">
                 </a>
