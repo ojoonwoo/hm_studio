e4685367fab1e71fde167754b3f8c51a9fac1de7
kyhfan <kyhfan@gmail.com>
2018-12-17 10:58:51 +0900

    버튼 트래킹 국문 PC 작업

--- index.php
+++ index.php
@@ -89,22 +89,22 @@
 			<div class="menu-layer">
 				<div class="spread-layer">
 					<div class="row">
-						<a href="javascript:void(0)" onclick="movePage(1);click_tracking('이동 CAMPAIGN FILM')">CAMPAIGN FILM</a>
+						<a href="javascript:void(0)" onclick="movePage(1);click_tracking('국문/이동 CAMPAIGN FILM')">CAMPAIGN FILM</a>
 					</div>
 					<div class="row">
-						<a href="javascript:void(0)" onclick="movePage(2);click_tracking('이동 EVENT')">EVENT</a>
+						<a href="javascript:void(0)" onclick="movePage(2);click_tracking('국문/이동 EVENT')">EVENT</a>
 					</div>
 					<div class="row">
-						<a href="javascript:void(0)" onclick="movePage(3);click_tracking('이동 EXPLORE THE POSSIBILITIES')">EXPLORE THE POSSIBILITIES</a>
+						<a href="javascript:void(0)" onclick="movePage(3);click_tracking('국문/이동 EXPLORE THE POSSIBILITIES')">EXPLORE THE POSSIBILITIES</a>
 					</div>
 					<div class="row">
-						<a href="javascript:void(0)" onclick="movePage(4);click_tracking('이동 HYUNDAI MOTORSTUDIO')">HYUNDAI MOTORSTUDIO</a>
+						<a href="javascript:void(0)" onclick="movePage(4);click_tracking('국문/이동 HYUNDAI MOTORSTUDIO')">HYUNDAI MOTORSTUDIO</a>
 					</div>
 					<div class="row share">
-						<a href="javascript:void(0)" class="fb" onclick="click_tracking('공유 페이스북');sns_share('fb')"></a>
-						<a href="javascript:void(0)" class="kt" onclick="click_tracking('공유 카카오톡');sns_share('kt')"></a>
-						<a href="javascript:void(0)" class="ks" onclick="click_tracking('공유 카카오스토리');sns_share('ks')"></a>
-						<a href="javascript:void(0)" class="url" id="copyUrl" onclick="click_tracking('공유 URL');"></a>
+						<a href="javascript:void(0)" class="fb" onclick="click_tracking('국문/공유 페이스북');sns_share('fb')"></a>
+						<a href="javascript:void(0)" class="kt" onclick="click_tracking('국문/공유 카카오톡');sns_share('kt')"></a>
+						<a href="javascript:void(0)" class="ks" onclick="click_tracking('국문/공유 카카오스토리');sns_share('ks')"></a>
+						<a href="javascript:void(0)" class="url" id="copyUrl" onclick="click_tracking('국문/공유 URL');"></a>
 					</div>
 				</div>
 			</div>
@@ -118,7 +118,7 @@
 								<h3>당신의 가능성을 탐험하라</h3>
 							</div>
 							<div class="button-wrap">
-								<button type="button" onclick="viewVideo();">
+								<button type="button" onclick="viewVideo();click_tracking('국문/풀 영상 보기');">
 									<p>Watch</p>
 									<p>Full Version</p>
 								</button>
@@ -152,7 +152,7 @@
 						<h4>해시태그 예시</h4>
 						<h4>#가능성탐험 #현대모터스튜디오 #모스크바</h4>
 					</div>
-					<button type="button" id="copyHashtag" onclick="click_tracking('복사 해시태그')">해시태그 복사하기</button>
+					<button type="button" id="copyHashtag" onclick="click_tracking('국문/복사 해시태그')">해시태그 복사하기</button>
 					<div class="prize">
 						<div class="_1">
 							<h3 class="tt">당첨자</h3>
@@ -305,32 +305,32 @@
 							</div>
 							<div class="slider-area">
 								<div>
-									<a href="http://motorstudio.hyundai.com/goyang/at/info.do" target="_blank">
+									<a href="http://motorstudio.hyundai.com/goyang/at/info.do" target="_blank" onclick="click_tracking('국문/슬라이더 고양스튜디오')">
 										<img src="./images/studio_slide1.jpg" alt="">
 									</a>
 								</div>
 								<div>
-									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/seoul/overview.html" target="_blank">
+									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/seoul/overview.html" target="_blank" onclick="click_tracking('국문/슬라이더 서울스튜디오')">
 										<img src="./images/studio_slide2.jpg" alt="">
 									</a>
 								</div>
 								<div>
-									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/hanam" target="_blank">
+									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/hanam" target="_blank" onclick="click_tracking('국문/슬라이더 하남스튜디오')">
 										<img src="./images/studio_slide3.jpg" alt="">
 									</a>
 								</div>
 								<div>
-									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/digital" target="_blank">
+									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/digital" target="_blank" onclick="click_tracking('국문/슬라이더 디지털스튜디오')">
 										<img src="./images/studio_slide4.jpg" alt="">
 									</a>
 								</div>
 								<div>
-									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/beijing" target="_blank">
+									<a href="https://www.hyundai.com/kr/ko/brand/motorstudio/beijing" target="_blank" onclick="click_tracking('국문/슬라이더 베이징스튜디오')">
 										<img src="./images/studio_slide5.jpg" alt="">
 									</a>
 								</div>
 								<div>
-									<a href="https://www.hyundai.ru/motorstudio_moscow" target="_blank">
+									<a href="https://www.hyundai.ru/motorstudio_moscow" target="_blank" onclick="click_tracking('국문/슬라이더 모스크바스튜디오')">
 										<img src="./images/studio_slide6.jpg" alt="">
 									</a>
 								</div>
