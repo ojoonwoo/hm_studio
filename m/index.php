4d674ac345eb0e00ad6894863440cfa974d79f71
kyhfan <kyhfan@gmail.com>
2018-12-17 10:24:22 +0900

    2222

--- m/index.php
+++ m/index.php
@@ -460,7 +460,7 @@
 			selection.removeAllRanges();
 
 			document.body.removeChild(textarea);
-			alert("해시태그가 복사되었습니다");
+			alert("URL이 복사되었습니다");
 		});
 		
 		$('.js-burger-trigger').on('click', function() {
